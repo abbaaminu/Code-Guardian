45a0656672a479dd5c0fd015e8a937d14e85cb4a
abbaaminu <abbaaminub@gmail.com>
2026-07-23 10:10:05 +0100

    Update favicon to SecurePulse icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/securepulse-icon.svg", type: "image/svg+xml" },
+      { rel: "apple-touch-icon", href: "/securepulse-icon.svg" },
     ],
   }),
   shellComponent: RootShell,
