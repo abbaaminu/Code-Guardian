51bac2186856bfd16314421d45798dab529f9d89
abbaaminu <abbaaminub@gmail.com>
2026-07-23 11:40:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      deployment: {
-        preset: 'vercel'
-      }
-    }),
+    tanstackStart(),
+    tailwindcss(),
     tsconfigPaths()
   ]
 })
